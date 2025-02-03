8329783483cabfe5e937227b9f385b9f8727927f
Pedro Cunha <pedro10cunha@gmail.com>
2025-02-03 03:08:36 +0000

    feat: Added InkWell (onTap) to FilledCard

--- lib/ui/widgets/card.dart
+++ lib/ui/widgets/card.dart
@@ -4,12 +4,14 @@ class FilledCard extends StatelessWidget {
   final IconData? icon;
   final String? title;
   final List<Widget> children;
+  final Function()? onTap;
 
   const FilledCard({
     super.key,
     required this.children,
     this.icon,
     this.title,
+    this.onTap,
   });
 
   @override
@@ -18,39 +20,44 @@ class FilledCard extends StatelessWidget {
     return Card.filled(
         color: Theme.of(context).colorScheme.surfaceContainer,
         margin: const EdgeInsets.symmetric(vertical: 6),
-        child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if(title != null && title!.isNotEmpty)
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 6),
-                          child: Icon(
-                            icon,
-                            size: 16,
-                            color: Theme.of(context).colorScheme.primary,
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          onTap: onTap,
+          child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if(title != null && title!.isNotEmpty)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 6),
+                            child: Icon(
+                              icon,
+                              size: 16,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
                           ),
-                        ),
 
-                        Text(
-                            title!,
-                            style: TextStyle(
-                                fontSize: 20.0,
-                                color: Theme.of(context).colorScheme.primary,
-                                fontWeight: FontWeight.bold
-                            )
-                        )
-                      ],
-                    ),
+                          Text(
+                              title!,
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold
+                              )
+                          )
+                        ],
+                      ),
 
-                  ...children
-                ]
-            )
+                    ...children
+                  ]
+              )
+          ),
         )
+
     );
   }
 }
