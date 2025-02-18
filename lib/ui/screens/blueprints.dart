998e876d2e1a2c51730dc33ae848e9772ec77e80
Pedro Cunha <pedro10cunha@gmail.com>
2025-02-18 12:38:09 +0000

    feat: Better blueprint viewer and new button added

--- lib/ui/screens/blueprints.dart
+++ lib/ui/screens/blueprints.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goipvc/models/blueprint.dart';
-
-import '../widgets/dropdown.dart';
+import 'package:photo_view/photo_view.dart';
 
 class BlueprintScreen extends StatefulWidget {
   const BlueprintScreen({super.key});
@@ -12,23 +11,12 @@ class BlueprintScreen extends StatefulWidget {
 
 class _BlueprintScreenState extends State<BlueprintScreen> {
   late int _currentIndex;
-  final TransformationController _transformationController = TransformationController();
   final DraggableScrollableController _sheetController = DraggableScrollableController();
   final List<Blueprint> blueprints = [
     Blueprint(
       index: 1,
       school: "ESTG",
       imageUrl: "https://picsum.photos/seed/1/1280/768",
-      legend: {
-        "SE": "Saída de Emergência",
-        "S": "Sala de Aula",
-        "L": "Laboratório",
-      },
-    ),
-    Blueprint(
-      index: 3,
-      school: "ESTG",
-      imageUrl: "https://picsum.photos/seed/3/1280/768",
       legend: {
         "SE": "Saída de Emergência",
         "S": "Sala de Aula",
@@ -45,12 +33,47 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
         "BL": "Biblioteca",
       },
     ),
+    Blueprint(
+      index: 3,
+      school: "ESTG",
+      imageUrl: "https://picsum.photos/seed/3/1280/768",
+      legend: {
+        "SE": "Saída de Emergência",
+        "S": "Sala de Aula",
+        "L": "Laboratório",
+      },
+    ),
   ];
 
   @override
   void initState() {
     super.initState();
     _currentIndex = 0;
+  }
+
+  Widget _buildSelector({
+        required Widget child,
+        required bool active,
+        required void Function() onTap
+      }){
+    return Container(
+      width: 42,
+      height: 42,
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      decoration: BoxDecoration(
+        color: active
+            ? Theme.of(context).colorScheme.primary
+            : Theme.of(context).colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(100),
+        onTap: onTap,
+        child: Center(
+          child: child,
+        ),
+      ),
+    );
   }
 
   Widget _buildLegendItem(String abbreviation, String description) {
@@ -93,48 +116,24 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
         builder: (context, constraints) {
           return Stack(
             children: [
-              InteractiveViewer(
-                transformationController: _transformationController,
-                boundaryMargin: const EdgeInsets.all(double.infinity),
-                minScale: 1,
+              PhotoView.customChild(
+                backgroundDecoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface
+                ),
+                minScale: 1.0,
                 maxScale: 10.0,
                 child: Center(
                   child: Image.network(
                     currentBlueprint.imageUrl,
                     fit: BoxFit.contain,
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
                   ),
-                ),
-              ),
-
-              Positioned(
-                left: 6,
-                top: 6,
-                child: Dropdown<String>(
-                  value: "ESTG",
-                  items: [
-                    DropdownMenuItem(
-                        value: "ESTG",
-                        child: Text(
-                            "ESTG",
-                          style: TextStyle(fontSize: 18),
-                        )
-                    ),
-                    DropdownMenuItem(
-                        value: "ESA",
-                        child: Text(
-                          "ESA",
-                          style: TextStyle(fontSize: 18),
-                        )
-                    ),
-                    DropdownMenuItem(
-                        value: "ESCE",
-                        child: Text(
-                          "ESCE",
-                          style: TextStyle(fontSize: 18),
-                        )
-                    ),
-                  ],
-                  onChanged: (Object? value) {  },
                 ),
               ),
 
@@ -144,12 +143,12 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
                 minChildSize: 0.1,
                 maxChildSize: 0.8,
                 snap: true,
-                snapSizes: const [0.1, 0.5, 0.8],
+                snapSizes: [0.1, 0.5, 0.8],
                 builder: (context, scrollController) {
                   return Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.surface,
-                      borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+                      color: Theme.of(context).colorScheme.surfaceContainer,
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
                       boxShadow: [
                         BoxShadow(
                           color: Theme.of(context).colorScheme.shadow.withOpacity(0.1),
@@ -173,7 +172,7 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          padding: EdgeInsets.symmetric(horizontal: 16),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -181,9 +180,12 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
                                 'Legenda - Piso ${currentBlueprint.index}',
                                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                               ),
-                              const SizedBox(height: 16),
+                              SizedBox(height: 16),
                               ...currentBlueprint.legend.entries.map(
-                                    (entry) => _buildLegendItem(entry.key, entry.value),
+                                    (entry) => _buildLegendItem(
+                                        entry.key,
+                                        entry.value
+                                    ),
                               ),
                             ],
                           ),
@@ -216,39 +218,47 @@ class _BlueprintScreenState extends State<BlueprintScreen> {
                       clipBehavior: Clip.hardEdge,
                       padding: const EdgeInsets.all(8),
                       child: Column(
-                        verticalDirection: VerticalDirection.up,
                         mainAxisSize: MainAxisSize.min,
-                        children: blueprints.asMap().entries.map((entry) {
-                          final index = entry.key;
-                          final blueprint = entry.value;
-                          return Container(
-                            width: 42,
-                            height: 42,
-                            margin: const EdgeInsets.symmetric(vertical: 4),
-                            decoration: BoxDecoration(
-                              color: _currentIndex == index
-                                  ? Theme.of(context).colorScheme.primary
-                                  : Theme.of(context).colorScheme.surfaceContainer,
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(100),
-                              onTap: () => setState(() => _currentIndex = index),
-                              child: Center(
-                                child: Text(
-                                  "${blueprint.index}",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: _currentIndex == index
-                                        ? Theme.of(context).colorScheme.onPrimary
-                                        : Theme.of(context).colorScheme.onSurface,
+                        children: [
+                          Column(
+                            verticalDirection: VerticalDirection.up,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              for(var (index, blueprint) in blueprints.indexed)
+                                _buildSelector(
+                                  active: _currentIndex == index,
+                                  child: Text(
+                                    "${blueprint.index}",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: _currentIndex == index
+                                          ? Theme.of(context).colorScheme.onPrimary
+                                          : Theme.of(context).colorScheme.onSurface,
+                                    ),
                                   ),
+                                  onTap: () => setState(() => _currentIndex = index),
                                 ),
+                            ],
+                          ),
+                          Container(
+                            height: 20,
+                            alignment: Alignment.center,
+                            child: Container(
+                              width: 32,
+                              height: 2,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.surfaceDim,
+                                borderRadius: BorderRadius.circular(2),
                               ),
                             ),
-                          );
-                        }).toList(),
+                          ),
+                          _buildSelector(
+                            active: false,
+                            child: Icon(Icons.swap_horiz),
+                            onTap: () => setState(() => _currentIndex = 0),
+                          )
+                        ],
                       ),
                     ),
                   );
