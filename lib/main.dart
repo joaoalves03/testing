2abfcacd5af2da00273b4a28f20c7a0dd98a1773
Matthew Rodrigues <mattcapazz@gmail.com>
2025-03-09 04:13:31 +0000

    feat(firebase): integrate Firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:goipvc/firebase_options.dart';
 import 'package:goipvc/themes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -16,8 +18,12 @@ import 'package:dynamic_color/dynamic_color.dart';
 
 final Logger logger = Logger();
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SharedPrefsUtil.initializeDefaults();
   runApp(
     ProviderScope(
